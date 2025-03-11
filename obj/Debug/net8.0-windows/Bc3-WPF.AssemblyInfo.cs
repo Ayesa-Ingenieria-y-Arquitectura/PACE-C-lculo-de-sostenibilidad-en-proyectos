4e6e02ea97875c5f1191e6dd0e001f8273670aed
Pablo Rodriguez <prodriguezsa@ayesa.com>
2025-03-11 09:51:03 +0100

    Fix Layout and Refactor C# code

--- obj/Debug/net8.0-windows/Bc3-WPF.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Bc3-WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bc3-WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f6648688b1b1ba024448a32f7b33c610d06c21f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e34281989f6d77e3bf58c4508274814833216a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bc3-WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bc3-WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
