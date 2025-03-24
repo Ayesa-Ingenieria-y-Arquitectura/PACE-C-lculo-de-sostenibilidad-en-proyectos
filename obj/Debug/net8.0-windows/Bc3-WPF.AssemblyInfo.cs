b5bce25d37cb5313a21321a9040a7528ac81a561
Pablo Rodriguez <prodriguezsa@ayesa.com>
2025-03-24 09:33:05 +0100

    Cambios esteticos

--- obj/Debug/net8.0-windows/Bc3-WPF.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Bc3-WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bc3-WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a2d1b0f2589dfc74f17f093fdeda6a82d137a1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04342e0255c676b2c9f6e7066e8154a4fea261ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bc3-WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bc3-WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
